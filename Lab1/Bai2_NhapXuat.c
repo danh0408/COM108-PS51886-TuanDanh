26bc42e47b8123c3c18a0a1f09bfea134f6c968d
unknown <nguyentuandanh014@gmail.com>
2026-09-22 22:10:34 +0700

    Hoan thanh Lab1 Bai 2

---
+++ Lab1/Bai2_NhapXuat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int namSinh; // %d
+    float diemTB; // %f
+
+    printf("Nhap nam sinh: ");
+    scanf("%d", &namSinh);
+    printf("Nhap diem: ");
+    scanf("%f", &diemTB);
+
+    // xuất 
+    printf("Nam sinh: %d\n", namSinh);
+    printf("Tuoi: %d\n", 2026 - namSinh);
+    printf("Diem TB: %f", diemTB);
+
+    return 0;
+}
